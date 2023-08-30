7872b4f1895089b2a6578a5e30db239a97e4b5f9
icamp <campan.ionel@gmail.com>
2023-08-30 15:10:56 +0300

    users list component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+const dummyUsers = [
+  {
+    id: '1',
+    name: 'jack',
+    age: 57,
+  },
+  {
+    id: '2',
+    name: 'spiderman',
+    age: 12,
+  },
+];
 
 function App() {
-  return <AddUser />;
+  return (
+    <div>
+      <AddUser />
+      <UsersList users={dummyUsers}/>
+    </div>
+  );
 }
 
 export default App;
